6a016d25e855a3a8bbb20c94a701fa952bf9b86f
Kirill Fertikov <kirill.fertikov@gmail.com>
2026-03-04 08:26:13 +0100

    implementation

--- src/ofxstatement_rs_altabanka/plugin.py
+++ src/ofxstatement_rs_altabanka/plugin.py
@@ -1,15 +1,35 @@
-from typing import Iterable
+import xml.etree.ElementTree as ET
+from datetime import datetime, date
+from decimal import Decimal
+from typing import Iterable, Optional
 
-from ofxstatement.plugin import Plugin
 from ofxstatement.parser import StatementParser
+from ofxstatement.plugin import Plugin
 from ofxstatement.statement import Statement, StatementLine
 
 
 class RsAltabankaPlugin(Plugin):
-    """Sample plugin (for developers only)"""
+    """Plugin for parsing AltaBanka statements"""
 
     def get_parser(self, filename: str) -> "RsAltabankaParser":
         return RsAltabankaParser(filename)
+
+
+def get_text(element: Optional[ET.Element], tag: str) -> str:
+    child = element.find(tag) if element is not None else None
+    return child.text.strip() if child is not None and child.text else ""
+
+
+def get_decimal(element: Optional[ET.Element], tag: str) -> Decimal:
+    text = get_text(element, tag)
+    return Decimal(text) if text else Decimal("0.0")
+
+
+def get_date(element: Optional[ET.Element], tag: str) -> Optional[date]:
+    text = get_text(element, tag)
+    if not text:
+        return None
+    return datetime.strptime(text[:10], "%Y-%m-%d").date()
 
 
 class RsAltabankaParser(StatementParser[str]):
@@ -18,13 +38,51 @@ class RsAltabankaParser(StatementParser[str]):
         self.filename = filename
 
     def parse(self) -> Statement:
-        """Main entry point for parsers
+        tree = ET.parse(self.filename)
+        root = tree.getroot()
 
-        super() implementation will call to split_records and parse_record to
-        process the file.
-        """
-        with open(self.filename, "r") as f:
-            return super().parse()
+        statement = Statement(
+            account_id=get_text(root, "acctid"),
+            currency=get_text(root, "curdef")
+        )
+
+        ledgerbal = root.find("ledgerbal")
+        statement.start_date = get_date(ledgerbal, "dtasof")
+        statement.start_balance = get_decimal(ledgerbal, "balamt")
+
+        availbal = root.find("availbal")
+        statement.end_date = get_date(availbal, "dtasof")
+        statement.end_balance = get_decimal(availbal, "balamt")
+
+        trnlist = root.find("trnlist")
+        if trnlist is not None:
+            statement.lines = [self.__parse_transation(trn) for trn in trnlist.findall("stmttrn")]
+
+        return statement
+
+    @staticmethod
+    def __parse_transation(stmttrn: ET.Element) -> StatementLine:
+        line = StatementLine(
+            id=get_text(stmttrn, "fitid"),
+            date=get_date(stmttrn, "dtposted"),
+            memo=get_text(stmttrn, "purpose"),
+            amount=get_decimal(stmttrn, "trnamt"),
+        )
+
+        payeeinfo = stmttrn.find("payeeinfo")
+        if payeeinfo is not None:
+            line.payee = get_text(payeeinfo, "name")
+
+        refnumber = get_text(stmttrn, "refnumber")
+        if refnumber:
+            line.refnum = refnumber
+
+        if get_text(stmttrn, "benefit") == "debit":
+            line.trntype = "DEBIT"
+        elif get_text(stmttrn, "benefit") == "credit":
+            line.trntype = "CREDIT"
+
+        return line
 
     def split_records(self) -> Iterable[str]:
         """Return iterable object consisting of a line per transaction"""
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,16 +1,81 @@
 import os
+from datetime import date
+from decimal import Decimal
 
 from ofxstatement.ui import UI
 
 from ofxstatement_rs_altabanka.plugin import RsAltabankaPlugin
 
 
-def test_sample() -> None:
+def test_sample():
     plugin = RsAltabankaPlugin(UI(), {})
     here = os.path.dirname(__file__)
-    sample_filename = os.path.join(here, "sample-statement.xml")
+    sample_filename = os.path.join(here, "resources/sample-statement.xml")
 
     parser = plugin.get_parser(sample_filename)
     statement = parser.parse()
 
     assert statement is not None
+    assert statement.account_id == "190-0000000000101-01"
+    assert statement.currency == "RSD"
+    assert statement.start_balance == Decimal("100017.74")
+    assert statement.start_date == date(2026, 3, 2)
+    assert statement.end_balance == Decimal("54787.52")
+    assert statement.end_date == date(2026, 3, 2)
+    assert len(statement.lines) == 7
+
+    txn_1 = statement.lines[0]
+    assert txn_1.id == "639845723402358"
+    assert txn_1.date == date(2026, 3, 2)
+    assert txn_1.memo == "Obračunata provizija u iznosu od 370.53 RSD"
+    assert txn_1.amount == Decimal("0.00")
+    assert txn_1.payee == "ALTA BANKA AD-račun provizije"
+    assert txn_1.trntype == "DEBIT"
+
+    txn_2 = statement.lines[1]
+    assert txn_2.id == "720934865346235"
+    assert txn_2.date == date(2026, 3, 2)
+    assert txn_2.memo == "DIN. PROTIVVREDNOST ZA :EUR 1000 KURS:115.5"
+    assert txn_2.amount == Decimal("115500.00")
+    assert txn_2.payee == "ALTA banka a.d. Beograd, Bulevar Zorana Đinđića 121,"
+    assert txn_2.trntype == "CREDIT"
+
+    txn_3 = statement.lines[2]
+    assert txn_3.id == "732098454734646"
+    assert txn_3.date == date(2026, 3, 2)
+    assert txn_3.memo == "Uplata javnih prihoda izuzev poreza i doprinosa po odbitku"
+    assert txn_3.amount == Decimal("120760.00")
+    assert txn_3.payee == "Poreska uprava, Save Maškovića 3-5,"
+    assert txn_3.trntype == "DEBIT"
+
+    txn_4 = statement.lines[3]
+    assert txn_4.id == "978324659823745"
+    assert txn_4.date == date(2026, 3, 2)
+    assert txn_4.memo == "POREZ NA PRIHOD 2026"
+    assert txn_4.amount == Decimal("25000.00")
+    assert txn_4.payee == "Poreska uprava, Save Maškovića 3-5,"
+    assert txn_4.trntype == "DEBIT"
+
+    txn_5 = statement.lines[4]
+    assert txn_5.id == "908423758493346"
+    assert txn_5.date == date(2026, 3, 2)
+    assert txn_5.memo == "Accounting services"
+    assert txn_5.amount == Decimal("10099.69")
+    assert txn_5.payee == "ACCOUNTING SERVICES,"
+    assert txn_5.trntype == "DEBIT"
+
+    txn_6 = statement.lines[5]
+    assert txn_6.id == "938745924653345"
+    assert txn_6.date == date(2026, 3, 2)
+    assert txn_6.memo == "Kartica 0000000000000000    : COMPANY.CO.RS               BEOGRAD"
+    assert txn_6.amount == Decimal("4500.00")
+    assert txn_6.payee == "ALTA BANKA AD BEOGRAD - DINA BUSSINES CA,"
+    assert txn_6.trntype == "DEBIT"
+
+    txn_7 = statement.lines[6]
+    assert txn_7.id == "623945693245734"
+    assert txn_7.date == date(2026, 3, 2)
+    assert txn_7.memo == "[AutoProv]Obracun provizije za dan 2026.03.02"
+    assert txn_7.amount == Decimal("370.53")
+    assert txn_7.payee == "ALTA BANKA AD-račun provizije,"
+    assert txn_7.trntype == "DEBIT"
